8877c56a264eb837423b863dee2d61907f7a771b
thgs <theogpl57@gmail.com>
2022-03-29 13:57:17 +0100

    actually implement break time

--- pomodoro.php
+++ pomodoro.php
@@ -10,24 +10,18 @@ $pomodoroTimer = new Timer(25);
 $startedAt = $pomodoroTimer->start();
 e("Pomodoro started " . $startedAt->format('H:i:s'));
 
-$dotTimer = new Timer(1);
-$dotTimer->start();
-while (!$pomodoroTimer->hasFinished()) {
-    if ($input = fgetc(STDIN)) {
-        processInput($input);
-    }
-    usleep(1000000);
-    if ($dotTimer->hasFinished()) {
-        echo '.';
-        $dotTimer->start();
-    }
-}
+loop($pomodoroTimer);
 
 $breakTimer = new Timer(5);
-$breakStartedAt = $breakTimer = $breakTimer->start();
+$breakStartedAt = $breakTimer->start();
 
 e('');
 e("Break started " . $breakStartedAt->format('H:i:s'));
+
+loop($breakTimer);
+
+e('');
+e("Pomodoro session finished.");
 
 function processInput(string $input)
 {
@@ -44,4 +38,20 @@ function processInput(string $input)
 function e(string $text): void
 {
     echo $text . PHP_EOL;
+}
+
+function loop(Timer $timer)
+{
+    $dotTimer = new Timer(1);
+    $dotTimer->start();
+    while (!$timer->hasFinished()) {
+        if ($input = fgetc(STDIN)) {
+            processInput($input);
+        }
+        usleep(1000000);
+        if ($dotTimer->hasFinished()) {
+            echo '.';
+            $dotTimer->start();
+        }
+    }
 }
